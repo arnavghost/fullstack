6ca22e0f1587a901bd8457736b5f07652c569b0c
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-05 01:35:22 +0530

    github component

---
+++ React/07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React , { useEffect, useState } from 'react'
+
+function Github(){
+    const [data,setData] = useState([])
+    useEffect(() =>{
+        fetch('https://api.github.com/users/arnavghost')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    },[])
+    
+    return (
+        <div className="text-center bg-gray-600 text-white text-3xl p-4">
+          <p>Github followers: {data.followers}</p>
+          <img
+            className=""
+            src={data.avatar_url}
+            alt="Git picture"
+            width={300}
+          />
+        </div>
+      );
+      
+}
+
+export default Github
