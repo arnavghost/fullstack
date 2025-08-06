444a571596d988dad08a8d96570fb7f598285592
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-06 15:12:40 +0530

    has the browser routes

---
+++ React/07reactRouter/src/main.jsx
@@ -0,0 +1,64 @@
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import './index.css'
+import Home from './components/Home/Home.jsx';
+import Layout from './Layout.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import User from './components/User/User.jsx';
+import Github, { githubInfoLoader } from './components/Github/Github.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',   //called as the root
+    element: <Layout />,//after putting the layout all of the other component renders 
+    children:[
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About /> 
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "user/:userid",
+        element: <User />
+      },
+      {
+        path: "github",
+        element: <Github />,
+        loader: githubInfoLoader,  
+      },
+    ]
+  }
+]);
+
+//other way to do it 
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route index element={<Home />} />
+//       <Route path="about" element={<About />} />//internal routing can be done 
+//       <Route path="contact" element={<Contact />} />
+//       <Route path="user/:userid" element={<User />} />
+//       <Route path="github" element={<Github />} />
+//     </Route>
+//   )
+// );
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
+
+
+export default router
