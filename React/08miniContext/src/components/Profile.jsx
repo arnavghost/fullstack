c3c0afd2ae22e44f076158bb687c35e64fa50f9f
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-06 16:53:53 +0530

    Add Profile component to display current user info

---
+++ React/08miniContext/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+//making this to see how data is accessed
+
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext); 
+
+  if (!user) return <div>Please login</div>;
+
+  return <div>Welcome {user.username}</div>;
+}
+
+export default Profile;
