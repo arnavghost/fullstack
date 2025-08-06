43622df5934091d992d7b48e1554177de864716d
arnavghost <arnavkhandelwal1464@gmail.com>
2025-08-06 15:06:26 +0530

    Github component

--- React/07reactRouter/src/components/Github/Github.jsx
+++ React/07reactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,17 @@
 import React , { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom';
 
 function Github(){
-    const [data,setData] = useState([])
-    useEffect(() =>{
-        fetch('https://api.github.com/users/arnavghost')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    },[])
+  const data= useLoaderData()
+    // const [data,setData] = useState([])
+    // useEffect(() =>{
+    //     fetch('https://api.github.com/users/arnavghost')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
     
     return (
         <div className="text-center bg-gray-600 text-white text-3xl p-4">
@@ -26,3 +28,8 @@ function Github(){
 }
 
 export default Github
+
+export const githubInfoLoader = async() => {
+  const response = await fetch('https://api.github.com/users/arnavghost')
+  return response.json()
+}
