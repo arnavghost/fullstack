e508f280d27d23f0f2c708fc54cc909aa63a2f1e
arnavghost <arnavkhandelwal1464@gmail.com>
2025-09-03 14:19:31 +0530

    add examples of React element creation with JSX and createElement

--- React/01vitereact/src/main.jsx
+++ React/01vitereact/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
+// A simple custom React component (not used below, just for demo)
+// This is the same as writing <MyApp /> in JSX
 function MyApp(){
     return (
         <div>
@@ -11,32 +13,54 @@ function MyApp(){
     )
 }
 
+// ---------------------------
+// EXAMPLE OF A REACT ELEMENT AS A PLAIN OBJECT (commented out)
+// ---------------------------
 // const ReactElement = {
-//     type: 'a',
-//     props: {
-//         href: 'https://google.com',
-//         target: '_blank'
+//     type: 'a', // means we want an <a> tag
+//     props: {   // props are attributes of the tag
+//         href: 'https://google.com',  // where the link goes
+//         target: '_blank'             // open link in a new tab
 //     },
-//     children: 'Click me to visit google'
+//     children: 'Click me to visit google' // the inner text of the <a>
 // }
+// This is basically how React internally represents JSX like:
+// <a href="https://google.com" target="_blank">Click me to visit google</a>
 
+
+// ---------------------------
+// JSX WAY of writing a React element
+// ---------------------------
 const anotherElement = (
     <a href="https://google.com" target='_blank'>Visit google</a>
 )
+// This is syntactic sugar for React.createElement()
+// React will convert it into an object similar to ReactElement above.
 
 
-
+// Just a string variable (not used in render below)
 const anotherUser = "chai aur react"
 
-const reactElement = React.createElement(
-    'a',
-    {href: 'https://google.com',target: '_blank' },
-    'click me to visit google',
-    anotherElement
-)
 
+// ---------------------------
+// Using React.createElement manually
+// ---------------------------
+const reactElement = React.createElement(
+    'a',  // type of element → <a>
+    { href: 'https://google.com', target: '_blank' }, // props (attributes)
+    'click me to visit google',  // child (inner text)
+    anotherElement                // another child (the JSX <a> above)
+)
+// This will create something like:
+// <a href="https://google.com" target="_blank">
+//    click me to visit google
+//    <a href="https://google.com" target="_blank">Visit google</a>
+// </a>
+
+
+// ---------------------------
+// Rendering into the root div in index.html
+// ---------------------------
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    reactElement
-  
+    reactElement // rendering the element created manually
 )
